5951cbbfa538f60875f5e9bc654c6daa96af220a
dENQ <qeqedenq@gmail.com>
2017-02-21 23:46:14 +0100

    -

---
+++ index.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Drago Monteks - Fasade - Slavonski Brod</title>
+<meta charset="utf-8">
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+<link rel="shortcut icon" type="image/png" href="public/img/favicon.ico"/>
+<meta http-equiv="content-LANGUAGE" content="hr">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="description" content="Izrada fasada Slavonski Brod, Brodsko Posavska županija">
+<meta name="keywords" content="fasade, fasada, slavonski brod, brodsko posavska, brodsko-posavska, drago monteks, moleraj, zbukanje" />
+<meta name="author" content="Denis Udovcic">
+<meta name="google-site-verification" content="Code from Google Webmaster tools"/>
+<meta name="ROBOTS" content="INDEX,FOLLOW">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css"  href="./public/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="./public/fonts/font-awesome/css/font-awesome.css">
+
+<!-- Stylesheet
+    ================================================== -->
+<link rel="stylesheet" type="text/css"  href="./public/css/style.css">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<!-- Navigation -->
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse"> <i class="fa fa-bars"></i> </button>
+      <a class="navbar-brand page-scroll" href="#page-top"> <i class="fa fa-paper-plane-o"></i> DRAGO MONTEKS</a> </div>
+    
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+      <ul class="nav navbar-nav">
+        <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+        <li class="hidden"> <a href="#page-top"></a> </li>
+        <li> <a class="page-scroll" href="#about">O nama</a> </li>
+        <li> <a class="page-scroll" href="#services">Usluge</a> </li>
+        <li> <a class="page-scroll" href="#works">Radovi</a> </li>
+        <li> <a class="page-scroll" href="#testimonials">Partneri</a> </li>
+        <li> <a class="page-scroll" href="#contact">Kontakt</a> </li>
+      </ul>
+    </div>
+    <!-- /.navbar-collapse --> 
+  </div>
+  <!-- /.container --> 
+</nav>
+
+<!-- Header -->
+<div id="intro">
+  <div class="intro-body">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <h1>DRAGO <span class="brand-heading">MONTEKS</span></h1>
+          <p class="intro-text">vl. Drago Udovčić</p>
+          <a href="#about" class="btn btn-default page-scroll">Više...</a> </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- About Section -->
+<div id="about">
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>O nama</h2>
+      <hr>
+    </div>
+    <div class="row">
+      <div class="col-md-6"><img src="./public/img/about.jpg" class="img-responsive"></div>
+      <div class="col-md-6">
+        <div class="about-text">
+          <h4>Tko smo mi ?</h4>
+          <p>Građevinski obrt Drago Monteks osnovan je 1998. godine u Slavonskom Brodu. Bavimo se izradom fasade ( ETICS sustav ), strojnim žbukanjem i suhom gradnjom.</p>
+          <p>.</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="about-text">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Services Section -->
+<div id="services" class="text-center">
+  <div class="container">
+    <div class="section-title center">
+      <h2>Usluge koje nudimo</h2>
+      <hr>
+    </div>
+    <div class="space"></div>
+    <div class="row">
+      <div class="col-md-3 col-sm-6">
+        <div class="service"> <i class="fa fa-desktop"></i>
+          <h3>Izrada fasade</h3>
+          <p>Izvodimo sve vrste fasaderskih radova po uputama proizvođača, na sve izvršene radove dajemo garanciju.</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class="service"> <i class="fa fa-cogs"></i>
+          <h3>Strojno žbukanje</h3>
+          <p>Cementna ili gipsana žbuka.</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class="service"> <i class="fa fa-tablet"></i>
+          <h3>Moleraj</h3>
+          <p>Unutarnje i vanjsko bojanje ( fasade ).</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class="service"><i class="fa fa-html5"></i>
+          <h3>Suha gradnja</h3>
+          <p>Izrađujemo potkrovlja, pregradne stijene i spuštene stropove.</p>
+        </div>
+      </div>
+    </div>
+    <div class="space"></div>
+    <div class="row">
+    <div class="col-md-3 col-lg-12">
+      <div class="service"><i class="i"></i>
+      <h3 id="posebno">Posebno u ponudi</h3>
+      <p id="posebnop">Izrada ponuda za prijavu na natječaj FONDA ZAŠTITE OKOLIŠA I ENERGETSKE UČINKOVITOSTI.</p>
+    </div>
+    </div>
+  </div>
+</div>
+<!-- Portfolio Section -->
+<div id="works">
+  <div class="container"> <!-- Container -->
+    <div class="section-title text-center center">
+      <h2>Primjeri radova</h2>
+      <hr>
+      <div class="clearfix"></div>
+    </div>
+    <div class="categories">
+      <ul class="cat">
+        
+      </ul>
+      <div class="clearfix"></div>
+    </div>
+    <div class="row">
+      <div class="portfolio-items">
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 lorem">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="./public/img/portfolio/01.jpg" title="Primjer fasade 1" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Primjerak završene fasade</h4>
+                
+              </div>
+              <img src="./public/img/portfolio/01.jpg" class="img-responsive" alt="Primjer fasade 1"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 consectetur">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="./public/img/portfolio/02.jpg" title="Primjer fasade 2" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Primjerak završene fasade</h4>
+                
+              </div>
+              <img src="./public/img/portfolio/02.jpg" class="img-responsive" alt="Primjer fasade 2"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 lorem">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="./public/img/portfolio/03.jpg" title="Primjer fasade 3" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Primjerak završene fasade</h4>
+                
+              </div>
+              <img src="./public/img/portfolio/03.jpg" class="img-responsive" alt="Primjer fasade 3"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 lorem">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="./public/img/portfolio/04.jpg" title="Primjer fasade 4" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Primjerak završene fasade</h4>
+                
+              </div>
+              <img src="./public/img/portfolio/04.jpg" class="img-responsive" alt="Primjer fasade 4"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 consectetur">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="./public/img/portfolio/05.jpg" title="Primjer fasade 5" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Primjerak završene fasade</h4>
+                
+              </div>
+              <img src="./public/img/portfolio/05.jpg" class="img-responsive" alt="Primjer fasade 5"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 dapibus">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="./public/img/portfolio/06.jpg" title="Primjer fasade 6" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Primjerak završene fasade</h4>
+                
+              </div>
+              <img src="./public/img/portfolio/06.jpg" class="img-responsive" alt="Primjer fasade 6"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 dapibus consectetur">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="./public/img/portfolio/07.jpg" title="Primjer fasade 7" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Primjerak završene fasade</h4>
+                
+              </div>
+              <img src="./public/img/portfolio/07.jpg" class="img-responsive" alt="Primjer fasade 7"> </a> </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 lorem">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="./public/img/portfolio/08.jpg" title="Primjer fasade 8" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Primjerak završene fasade</h4>
+                
+              </div>
+              <img src="./public/img/portfolio/08.jpg" class="img-responsive zero" alt="Primjer fasade 8"> </a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Testimonials Section -->
+<div id="testimonials" class="text-center">
+  <div class="container">
+    <div class="section-title center">
+      <h2>S kime surađujemo</h2>
+      <hr>
+    </div>
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+        <div class="row testimonials">
+          <div class="col-sm-6">
+            <blockquote><i class="fa fa-quote-left"></i>
+              <img src="./public/img/metalproizvod.png" rel="metalproizvod" class="metalproizvod">
+              <div class="clients-name">
+                <p><strong>Metalproizvod d.o.o.</strong><br>
+              </div>
+            </blockquote>
+          </div>
+          <div class="col-sm-6">
+            <blockquote><i class="fa fa-quote-left"></i>
+              <img src="./public/img/sitolor.png" rel="sitolor" class="sitolor">
+              <div class="clients-name">
+                <p><strong>Sitolor</strong><br>
+              </div>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Contact Section -->
+<div id="contact" class="text-center">
+  <div class="container">
+    <div class="section-title center">
+      <h2>Kontakt</h2>
+      <hr>
+      <p id="kontakt-tekst">Za sve informacije koje vas zanimaju, besplatan savjet ili besplatnu ponudu slobodno nas kontaktirajte.</p>
+    </div>
+    <div class="col-md-8 col-md-offset-2">
+      <div class="col-md-4">
+        <div class="contact-item"> <i class="fa fa-map-marker fa-2x"></i>
+          <p>Dubrovačka 14,<br>
+            Slavonski Brod 35000</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="contact-item"> <i class="fa fa-envelope-o fa-2x"></i>
+          <p>udovcic@net.hr</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="contact-item"> <i class="fa fa-phone fa-2x"></i>
+          <p> +385 98 709 601</p>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+    <div class="col-md-8 col-md-offset-2">
+      <form name="sentMessage" id="contactForm" novalidate>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="text" id="name" class="form-control" placeholder="Ime" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="email" id="email" class="form-control" placeholder="Email" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <textarea name="message" id="message" class="form-control" rows="4" placeholder="Poruka" required></textarea>
+          <p class="help-block text-danger"></p>
+        </div>
+        <div id="success"></div>
+        <button type="submit" class="btn btn-default">Pošalji poruku</button>
+      </form>
+      <div class="social">
+      </div>
+      <iframe width="600" height="450" frameborder="0" style="border:0"
+src="https://www.google.com/maps/embed/v1/place?q=dubrova%C4%8Dka%2014%20slavonski%20brod&key=AIzaSyDvS4op0TecEdqs5lccCADMBhsxaTwdFpA" allowfullscreen></iframe>
+    </div>
+  </div>
+</div>
+
+<!--
+<div id="footer">
+  <div class="container">
+    <p>Copyright &copy; Modus. Designed by <a href="http://www.templatewire.com" rel="nofollow">TemplateWire</a></p>
+  </div>
+</div>
+
+-->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script type="text/javascript" src="./public/js/jquery.1.11.1.js"></script> 
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script type="text/javascript" src="./public/js/bootstrap.js"></script> 
+<script type="text/javascript" src="./public/js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="./public/js/jquery.prettyPhoto.js"></script> 
+<script type="text/javascript" src="./public/js/jquery.isotope.js"></script> 
+<script type="text/javascript" src="./public/js/jquery.parallax.js"></script> 
+<script type="text/javascript" src="./public/js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="./public/js/contact_me.js"></script> 
+
+<!-- Javascripts
+    ================================================== --> 
+<script type="text/javascript" src="./public/js/main.js">
+</script>
+<link rel="stylesheet" type="text/css" href="./public/css/prettyPhoto.css">
+
+</body>
+</html>
